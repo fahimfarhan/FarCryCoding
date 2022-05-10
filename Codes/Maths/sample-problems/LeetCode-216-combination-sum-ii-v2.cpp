02ddaffc2f55f51900bfd1a051ee82eb1367f19b
Soumic <fahim.farhan@outlook.com>
2022-05-10 15:37:22 +0600

    :hammer_and_pick: #tags backtrack, math, generating_combinations

--- Codes/Maths/sample-problems/LeetCode-216-combination-sum-ii-v2.cpp
+++ Codes/Maths/sample-problems/LeetCode-216-combination-sum-ii-v2.cpp
@@ -9,13 +9,12 @@ public:
       this->n = n;
       this->ssf = 0;
 
-  vector<vector<int> > result;
-  vector<int> path;
-  vector<bool> isVisited(10, false);
-  
+      isVisited.resize(10, false);
+      path.clear();
+      result.clear();
 
       for(int i=1; i<10; i++) {
-        backTrack(i, 1, path, isVisited, result);
+        backTrack(i, 1);
       }
 
       return result;
@@ -24,43 +23,35 @@ public:
 
 private:
   int k, n, ssf;
-  void backTrack(const int& u, int h, 
-  vector<int>& path,
-  vector<bool>& isVisited,
-  vector<vector<int> >& result
-  ) {
+  vector<vector<int> > result;
+  vector<int> path;
+  vector<bool> isVisited;
+  
+  void backTrack(const int& u, int h) {
     if(h>k) {
       return;
     }
     
-    push(u, path, isVisited, result);
+    push(u);
     if( (h == k) && (ssf == n) ) {
       vector<int> someClone(path);
       result.push_back(someClone);
     } else {
       int h1 = h + 1;
       for(int v=u+1; v<10; v++) {
-        backTrack(v, h1, path, isVisited, result);
+        backTrack(v, h1);
       }
     }
-    pop(u, path, isVisited, result);
+    pop(u);
   }
 
-  void push(const int& u, 
-    vector<int>& path,
-    vector<bool>& isVisited,
-    vector<vector<int> >& result
-  ) {
+  void push(const int& u) {
     isVisited[u] = true;
     path.push_back(u);
     ssf += u;
   }
 
-  void pop(const int& u, 
-    vector<int>& path,
-    vector<bool>& isVisited,
-    vector<vector<int> >& result
-  ) {
+  void pop(const int& u) {
     isVisited[u] = false;
     path.pop_back();
     ssf-=u;
